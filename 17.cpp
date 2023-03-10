fa2cebef4aa1415657ff514185ae05af0cfcb885
Angelina <99400952+Anglnnn@users.noreply.github.com>
2023-03-10 21:23:39 +0200

    Add files via upload

---
+++ 17.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> Obchislennya(int SIZE)
+{
+	bool *isPrime = new bool[SIZE + 1];
+	for (int i = 0; i < SIZE + 1; i++) {
+		isPrime[i] = true;
+	}
+	for (int p = 2; pow(p,2) <= SIZE; p++) 
+		if (isPrime[p]) 
+			for (int i = pow(p,2); i <= SIZE; i += p) 
+				isPrime[i] = false;
+
+	vector<int> prime;
+	for (int i = 2; i <= SIZE; i++) 
+		if (isPrime[i]) 
+			prime.push_back(i);
+	
+	delete[] isPrime;
+	return prime;
+}
+bool check(int A[], int P, int N)
+{
+	if (A[0] % P == 0)
+		return 0;
+
+	for (int i = 1; i < N; i++)
+		if (A[i] % P)
+			return 0;
+
+	if (A[N - 1] % (P * P) == 0)
+		return 0;
+
+	return 1;
+}
+bool checkIrreducibilty(int A[], int N)
+{
+	int M = -1;
+
+	for (int i = 0; i < N; i++) {
+		M = max(M, A[i]);
+	}
+
+	vector<int> primes
+		= Obchislennya(M + 1);
+
+	for (int i = 0;
+		i < primes.size(); i++) {
+
+		if (check(A, primes[i], N)) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Ukr");
+	const int SIZE = 4;
+	int arr[4] = { 4, 7, 21, 28 }; // стандартні значення
+	//cout << "Введіть значення: ";
+	/*for (int i = 0; i < SIZE; i++) {
+		cin >> arr[i];
+	}*/
+	cout << checkIrreducibilty(arr, SIZE);
+}
---
+++ 7.cpp
@@ -0,0 +1,51 @@
+﻿#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+int StepinMod(int x, int y, int p)
+{
+	int result = 1; 
+	x = x % p; 
+	do
+	{
+		if (y & 1) {
+			result = (result * x) % p;
+		}
+		y = y >> 1;
+		x = (x * x) % p;
+	} while (y);
+	return result;
+}
+
+int Diskretniy_Lohorifm(int a, int b, int m) {
+
+	unordered_map<int, int> znachennya;
+	int step = (int)sqrt(m) + 1;
+	for (int i = step; i >= 1; --i) {
+		znachennya[StepinMod(a, i * step, m)] = i;
+	}
+	for (int j = 0; j < step; ++j)
+	{
+		int cur = (StepinMod(a, j, m) * b) % m;
+		if (znachennya[cur])
+		{
+			int ans = znachennya[cur] * step - j;
+			if (ans < m)
+				return ans;
+		}
+	}
+	return -1;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Ukr");
+	int a, b, m;
+	cout << "Введіть a: ";
+	cin >> a;
+	cout << "Введіть b: ";
+	cin >> b;
+	cout << "Введіть m: ";
+	cin >> m;
+	cout << Diskretniy_Lohorifm(a, b, m) << endl;
+}
